81cab7afc434ea42399678b2b14e764badf4a4db
Emile <bvcxzq1@gmail.com>
2023-10-03 19:11:22 +0000

    sidebar challenge, css

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,5 +1,8 @@
+import React from "react"
+import css from "./css/Sidebar.module.css"
+
 const Sidebar = () => {
-    return <div><a href="#">My Photos</a><a href="#">My Illustations</a><a href="#">My Paintings</a></div>
+    return <div className = {css.sidebar}><a href="#">My Photos</a><a href="#">My Illustations</a><a href="#">My Paintings</a></div>
 };
 
 export default Sidebar;
