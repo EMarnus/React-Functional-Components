308a9a31ab80ca1fedee6f1e3e1b31a6c54bfb5f
Emile <bvcxzq1@gmail.com>
2023-10-04 17:12:08 +0000

    ES7 Snippets

---
+++ my-app/src/components/ES7Spinits.js
@@ -0,0 +1,53 @@
+// rfce
+// import React from 'react'
+
+// function ES7Spinits() {
+//   return (
+//     <div>ES7Spinits</div>
+//   )
+// }
+
+// export default ES7Spinits
+// end
+
+// rafce
+// import React from 'react'
+
+// const ES7Spinits = () => {
+//   return (
+//     <div>ES7Spinits</div>
+//   )
+// }
+
+// export default ES7Spinits
+// end
+
+// rce
+// import React, { Component } from 'react'
+
+// export class ES7Spinits extends Component {
+//   render() {
+//     return (
+//       <div>ES7Spinits</div>
+//     )
+//   }
+// }
+
+// export default ES7Spinits
+// end
+
+// rconst
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+// end
+
+// clg
+// console.log(first)
+
+// clo
+// console.log('first', first)
