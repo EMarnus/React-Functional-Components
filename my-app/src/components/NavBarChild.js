d985c3c0caac42c883629185fe3556381da51f88
Emile <bvcxzq1@gmail.com>
2023-10-07 14:50:56 +0000

    loading animation

--- my-app/src/components/NavBarChild.js
+++ my-app/src/components/NavBarChild.js
@@ -7,10 +7,10 @@ function NavBarChild(props) {
         :
         <form>
             <label htmlFor = "username">Username: </label>
-            <input placeholder="username" id="username"/>
+            <input placeholder="username" id="username" name="username"/>
         
             <label htmlFor = "password">Password: </label>
-            <input placeholder="password" id="password"/>
+            <input placeholder="password" id="password" name="password"/>
             <button  onClick={props.handleButtonClick}>Submit</button>
         </form>
     )
